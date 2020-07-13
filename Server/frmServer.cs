cd1a9cf61dc518fa8ce6f839ee68a99a903a41a8
aliyasindogan <a.yasindogan@gmail.com>
2020-07-14 01:13:03 +0300

    CodeMade programı ile using ve boşluklar silindi.

--- Server/frmServer.cs
+++ Server/frmServer.cs
@@ -1,14 +1,10 @@
-﻿using Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Server
